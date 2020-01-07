bb15ba8bcae854fc96b274ba5b8d01f2deaf8264
dinkotom <dinkotom@users.noreply.github.com>
2020-01-07 13:23:42 +0100

    Added button assignments for MCP and Yoke

--- KA350.lua
+++ KA350.lua
@@ -10,12 +10,46 @@ create_switch(169, "sim/cockpit2/controls/gear_handle_down", 0, 1, 0)
 -- AUTOPILOT ALTITUDE
 create_positive_edge_increment(163,"sim/cockpit/autopilot/altitude",0,1000,49000,0)
 create_positive_edge_decrement(162,"sim/cockpit/autopilot/altitude",0,1000,1000,0)
+create_positive_edge_increment(160,"sim/cockpit/autopilot/altitude",0,100,49000,0)
+create_positive_edge_decrement(161,"sim/cockpit/autopilot/altitude",0,100,1000,0)
 
 -- AUTOPILOT HEADING
-create_positive_edge_increment(177,"sim/cockpit/autopilot/heading_mag",0,10,400,0)
-create_positive_edge_decrement(176,"sim/cockpit/autopilot/heading_mag",0,10,-100,0)
+create_positive_edge_increment(179,"sim/cockpit/autopilot/heading_mag",0,10,400,0)
+create_positive_edge_decrement(178,"sim/cockpit/autopilot/heading_mag",0,10,-100,0)
+create_positive_edge_increment(177,"sim/cockpit/autopilot/heading_mag",0,1,400,0)
+create_positive_edge_decrement(176,"sim/cockpit/autopilot/heading_mag",0,1,-100,0)
 
 set("sim/cockpit/autopilot/heading_mag", -100)
+
+-- MCP
+set_button_assignment(186, "KA350/cmd/cPanel/efisSCP/courseDec")
+set_button_assignment(187, "KA350/cmd/cPanel/efisSCP/courseInc")
+set_button_assignment(184, "sim/autopilot/heading_sync")
+set_button_assignment(181, "KA350/cmd/cPanel/flightCP/nav")
+set_button_assignment(182, "KA350/cmd/cPanel/flightCP/appr")
+set_button_assignment(185, "KA350/cmd/cPanel/flightCP/bc")
+set_button_assignment(190, "KA350/cmd/cPanel/pilotDisplayCP/crsAct")
+set_button_assignment(189, "KA350/cmd/cPanel/flightCP/hdg")
+set_button_assignment(171, "KA350/cmd/cPanel/flightCP/climb")
+set_button_assignment(188, "KA350/cmd/cPanel/flightCP/altSel")
+set_button_assignment(172, "KA350/cmd/cPanel/flightCP/dsc")
+set_button_assignment(170, "KA350/cmd/cPanel/flightCP/alt")
+set_button_assignment(168, "KA350/cmd/cPanel/flightCP/vs")
+set_button_assignment(173, "KA350/cmd/cPanel/autopilotCP/apEng")
+set_button_assignment(167, "KA350/cmd/cPanel/autopilotCP/yawEng")
+
+-- YOKE
+set_button_assignment(325, "KA350/cmd/cPanel/autopilotCP/pitchSwitchDec")
+set_button_assignment(324, "KA350/cmd/cPanel/autopilotCP/pitchSwitchInc")
+set_button_assignment(323, "sim/flight_controls/pitch_trim_down")
+set_button_assignment(322, "sim/flight_controls/pitch_trim_up")
+set_button_assignment(326, "KA350/cmd/cPanel/autopilotCP/turnDec")
+set_button_assignment(327, "KA350/cmd/cPanel/autopilotCP/turnInc")
+set_button_assignment(321, "KA350/cmd/Yoke/AP_YD_ElecTrim_disc")
+
+
+
+
 
 do_every_frame([[
 
